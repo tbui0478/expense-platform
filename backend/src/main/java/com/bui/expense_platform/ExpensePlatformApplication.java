0cb9d2c6a2dac7f8c950f6ddda7872790f62bab8
tbui0478 <trinh.buii04@gmail.com>
2026-08-12 17:29:25 -0400

    Initial commit

---
+++ backend/src/main/java/com/bui/expense_platform/ExpensePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.bui.expense_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensePlatformApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bui/expense_platform/ExpensePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bui.expense_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
